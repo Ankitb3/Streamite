ee6a0722c162b77ee49165d7bffc3da6ee191b95
Ankit Bankar <bankarankit3@gmail.com>
2023-08-07 21:50:49 +0530

    home add

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,11 @@
 import Header from "../Components/Navbar/Navbar";
+import Slider from "../Components/Slider/Slider";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <Slider/>
     </div>
   );
 };
