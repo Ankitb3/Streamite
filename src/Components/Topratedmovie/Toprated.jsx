55e4c5a6ef3d9e3f778f9de031e201a06579dd47
Ankit Bankar <bankarankit3@gmail.com>
2023-08-08 02:25:04 +0530

    Add top rated component

---
+++ src/Components/Topratedmovie/Toprated.jsx
@@ -0,0 +1,43 @@
+import { useContext } from "react";
+import { Moviedata } from "../Context/Context";
+import { Container } from "react-bootstrap";
+import "./Toprated.css";
+const Toprated = () => {
+  const { filterData } = useContext(Moviedata);
+  console.log("top", filterData);
+  const highestRatingShow = filterData.filter((item) => {
+    return item.show.rating.average > 8;
+  });
+
+  const removetag = highestRatingShow[0]?.show?.summary
+    .replace(/<[^>]*>/g, "")
+    .slice(0, 80);
+  console.log(removetag);
+
+  return (
+    <Container>
+      <div className="main_top">
+        <img
+          src={highestRatingShow[0]?.show?.image.original}
+          alt="sdsds"
+          height={500}
+          className="Top_image"
+          style={{ float: "left", textAlign: "center", padding: "4px" }}
+        />
+        <h1
+          style={{ borderBottom: "2px solid #e40813", fontFamily: "cursive" }}
+        >
+          Top Rated Movie Of The Year
+        </h1>
+        <h2>{highestRatingShow[0]?.show?.name}</h2>
+        <p className="fs-4">{removetag}.</p>
+        <p className="fs-4">
+          Rating -{highestRatingShow[0]?.show?.rating.average}{" "}
+        </p>
+        <button className="button-2 p-3">Watch</button>
+      </div>
+    </Container>
+  );
+};
+
+export default Toprated;
